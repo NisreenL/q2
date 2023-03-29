124faaeb1a43a0a25f3652f51f7609f9d7f08edb
NisreenL <122961483+NisreenL@users.noreply.github.com>
2023-03-29 14:24:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Program {
+    
+static void Main(string[] args){
+        
+        Console.Write("N : ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("K : ");
+        string name = Console.ReadLine();
+        
+         if (number > 1 && number < 10000){
+                
+         }
+        
+}
+}
+        
